deacab7f7705cb075db0ed21c0b6618fb0d73e64
Joe Previte <jjprevite@gmail.com>
2021-10-09 07:58:39 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# streak-counter
-Created with CodeSandbox
+# Streak Counter
+
+A basic demo for a streak counter.
+
+## Future Ideas
+
+@maxcell had a great idea on stream -> streak based on occurences where you track based on the number of occurences per month.
---
+++ src/lib.ts
@@ -0,0 +1,71 @@
+import { differenceInDays } from "date-fns";
+
+type Streak = {
+  startDate: Date;
+  lastLoginDate: Date;
+  currentCount: number;
+};
+
+export function buildStreakCount(date: Date) {
+  return {
+    startDate: date,
+    lastLoginDate: date,
+    currentCount: 1
+  };
+}
+
+export function resetStreakCount(currentStreak: Streak, date: Date) {
+  return {
+    startDate: date,
+    lastLoginDate: date,
+    currentCount: 1
+  };
+}
+
+export function incrementStreakCount(currentStreak: Streak) {
+  return {
+    ...currentStreak,
+    currentCount: currentStreak.currentCount += 1
+  };
+}
+
+/**
+ *
+ * @param currentDate
+ * @param lastLoginDate
+ * returns a boolean value indicating whether or not you should increment or
+ * reset streak count
+ */
+export function shouldInrementOrResetStreakCount(
+  currentDate: Date,
+  lastLoginDate: Date
+) {
+  const difference = differenceInDays(currentDate, lastLoginDate);
+
+  // logging in on the same day
+  if (difference === 0) {
+    return {
+      shouldIncrement: false,
+      shouldReset: false
+    };
+  }
+  // This means they logged in the day after the current
+  if (difference === 1) {
+    return {
+      shouldIncrement: true,
+      shouldReset: false
+    };
+  }
+
+  // Otherwise they logged in after a day, which would
+  // break the streak
+  return {
+    shouldIncrement: false,
+    shouldReset: true
+  };
+}
+
+/*
+
+original stream: https://www.youtube.com/watch?v=ndBAg6lqlwI
+*/
