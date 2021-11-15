fb89ecf419620d95a5c66e8d4016c89d12cafea0
Joe <joe@coder.com>
2021-11-15 14:58:22 +0000

    refactor: add JSDoc block to useStreak

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,24 @@ import {
   incrementStreakCount,
 } from "./lib";
 
+/**
+ *
+ * @param {Storage} _localStorage - pass in `localStorage`
+ * @param {Date} currentDate - pass in current date i.e. `new Date()`
+ * @returns Streak - an object with `currentCount`, `lastLoginDate`, `startDate`
+ * 
+ * @example
+import { useStreak } from "use-streak";
+
+const today = new Date();
+const streak = useStreak(localStorage, today);
+// streak returns an object:
+// {
+//    currentCount: 1,
+//    lastLoginDate: "11/11/2021",
+//    startDate: "11/11/2021",
+// }
+ */
 export function useStreak(_localStorage: Storage, currentDate: Date) {
   // Check if streak exists
   const _doesStreakExist = doesStreakExist(_localStorage);
